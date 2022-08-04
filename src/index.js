58af45634a90d6cdc61303fbbc4f133c0e3ba99f
Ajay Gupta <ajay_gupta@live.com>
2022-08-04 12:25:50 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Demo from "./demo";
+
+const root = ReactDOM.createRoot(document.getElementById("container"));
+
+root.render(<Demo />);
